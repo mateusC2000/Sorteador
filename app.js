974fddefb869da71fecda784a280149b203e3ea9
Mateus <mateus.campos@rebase.com>
2023-10-13 00:55:49 -0300

    Update page title and add effect when choosing a number.

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-function draw() {
-  // const quantityNumbers = Number(document.querySelector('#quantity-numbers').value)
+async function draw() {
   const minNumber = Number(document.querySelector('#min-number').value)
   const maxNumber = Number(document.querySelector('#max-number').value)
-  console.log(maxNumber, minNumber)
-  const drawnNumber = Math.floor(Math.random() * maxNumber + minNumber)
 
-  let element = document.createElement('div')
-  element.classList.add('result-number')
-  element.innerText = drawnNumber
+  for (let i = 0; i < 50; i++) {
+    const resultNumber = document.getElementById('result-numbers')
+    resultNumber.innerHTML = ''
 
-  const resultNumber = document.getElementById('result-numbers')
-  resultNumber.innerHTML = ''
+    const drawnNumber = Math.floor(Math.random() * maxNumber + minNumber)
 
+    let element = document.createElement('div')
+    element.classList.add('result-number')
+    element.innerText = drawnNumber
 
-  resultNumber.appendChild(element)
+    resultNumber.appendChild(element)
+
+    await wait(50)
+  }
+}
+
+function wait(time) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), time)
+  })
 }
